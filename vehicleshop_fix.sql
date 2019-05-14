0a3bc9adc25584afffa007e067961e8c173d740d
William Plunkett Jr <joey.plunkett92@yahoo.com>
2019-05-14 14:13:41 -0500

    Update vehicleshop_fix.sql

--- vehicleshop_fix.sql
+++ vehicleshop_fix.sql
@@ -12,3 +12,18 @@ CREATE TABLE `owned_vehicles` (
 
 	PRIMARY KEY (`plate`)
 );
+
+-- My owned_vehicles that i use
+
+USE `essentialmode`;
+
+CREATE TABLE `owned_vehicles` (
+	`owner` varchar(30) NOT NULL,
+	`plate` varchar(12) NOT NULL,
+	`vehicle` longtext NOT NULL,
+	`type` VARCHAR(20) NOT NULL DEFAULT 'car',
+	`job` VARCHAR(20) NOT NULL,
+	`stored` TINYINT(1) NOT NULL DEFAULT '0',
+
+	PRIMARY KEY (`plate`)
+);
