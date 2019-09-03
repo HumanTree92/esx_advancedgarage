f99f9eabf2f32c81fcaf306831197e43ccae1d55
William Plunkett Jr <joey.plunkett92@yahoo.com>
2019-09-03 18:13:15 -0500

    Update vehicleshop_fix.sql

--- vehicleshop_fix.sql
+++ vehicleshop_fix.sql
@@ -1,29 +1,13 @@
--- Changed version of owned_vehicles to remove issues with car's not showing up
+-- This should fix the issue of the Car Garage not showing Vehicles.
 
 USE `essentialmode`;
 
-CREATE TABLE `owned_vehicles` (
-	`owner` varchar(22) NOT NULL,
-	`plate` varchar(12) NOT NULL,
-	`vehicle` longtext,
-	`type` VARCHAR(20) NOT NULL DEFAULT 'car',
-	`job` VARCHAR(20) NOT NULL DEFAULT '',
-	`stored` TINYINT(1) NOT NULL DEFAULT '0',
-
-	PRIMARY KEY (`plate`)
-);
-
--- My owned_vehicles that i use
-
-USE `essentialmode`;
-
-CREATE TABLE `owned_vehicles` (
-	`owner` varchar(30) NOT NULL,
-	`plate` varchar(12) NOT NULL,
-	`vehicle` longtext NOT NULL,
-	`type` VARCHAR(20) NOT NULL DEFAULT 'car',
-	`job` VARCHAR(20) NOT NULL,
-	`stored` TINYINT(1) NOT NULL DEFAULT '0',
-
-	PRIMARY KEY (`plate`)
+CREATE TABLE IF NOT EXISTS `owned_vehicles` (
+  `owner` varchar(30) NOT NULL,
+  `plate` varchar(12) NOT NULL,
+  `vehicle` longtext NOT NULL,
+  `type` varchar(20) NOT NULL DEFAULT 'car',
+  `job` varchar(20) NOT NULL DEFAULT '',
+  `stored` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`plate`)
 );
